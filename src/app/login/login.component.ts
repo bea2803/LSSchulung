3f82da4c01cc7ed09d95306a973fa42c07d0d0e4
Bea <bea.schoenknecht@outlook.de>
2022-04-06 16:55:08 +0200

    Angular done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -13,19 +14,20 @@ export class LoginComponent implements OnInit {
   errorMessage = 'Invalid Credentials'
   invalidLogin = false
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
 
   ngOnInit(): void {
   }
 
   handleLogin(){
-    if(this.username==="bea" && this.password ==="123") {
-      this.router.navigate(['welcome', this.username])
-      this.invalidLogin = false
-    } else {
+    // if(this.username==="bea" && this.password ==="123") {
+      if(this.hardcodedAuthenticationService.authenticate(this.username, this.password)){
+        this.router.navigate(['welcome', this.username])
+        this.invalidLogin = false
+      }
+     else {
       this.invalidLogin = true
     }
     
   }
-
 }
---
+++ src/app/service/hardcoded-authentication.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class HardcodedAuthenticationService {
+
+  constructor() { }
+
+  authenticate(username:string, password:string){
+    if(username==="bea" && password==="123") {
+      sessionStorage.setItem('authenticatedUser', username);
+      return true
+    }
+    else{
+      return false
+    }
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticatedUser')
+    return !(user === null)
+  }
+
+  logout(){
+    sessionStorage.removeItem('authenticatedUser')
+  }
+}
